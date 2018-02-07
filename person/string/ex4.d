b1c148a5c78385e4b3143987009858554360ba7a
Jean Zonta <wiki.anon@yahoo.com.br>
2018-02-07 20:04:11 -0200

    Sources code digital mars D

---
+++ person/string/ex4.d
@@ -0,0 +1,37 @@
+import std.stdio;
+import std.string;
+import std.traits;
+import std.range;
+import std.algorithm;
+
+auto read(S)(ref S s, string message)
+if (isSomeString!S) {
+    import std.string : strip;
+    writef("%s: ", message);
+    s = readln().strip();
+    return s;
+}
+
+auto read(S)(ref S s, string message)
+if (isStaticArray!S &&
+    isSomeChar!(ElementType!S)) {
+
+    string tmp;
+    read(tmp, message);
+
+    // Clear the array
+    s[] = typeof(s[0]).init;
+
+    // Assign without going out of bounds
+    const len = min(s.length, tmp.length);
+    s[0..len] = tmp[0..len];
+    //este readp irá ler apenas vetor de char.
+    return s;
+}
+
+void main()
+{
+    char[50] message;
+    read(message,"Digite Seu nome");
+    writeln(message);
+} 
