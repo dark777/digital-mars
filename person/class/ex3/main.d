edcfa6cafb01db5776b32556897cd58b53a4cd01
Jean Zonta <wiki.anon@yahoo.com.br>
2018-03-03 20:07:03 -0300

    Sources code digital mars D

--- person/class/ex3/main.d
+++ person/class/ex3/main.d
@@ -5,37 +5,35 @@ import include.adress; // Not Deprecated Adrees OK
 //import include.end.endereco; //Deprecated Endereco OK
 
 public static void main(string[] args)
-    {
-        Pessoa[] vetor = new Pessoa[1];
+{
+ Pessoa[] vetor = new Pessoa[1];
         
-        for(int i=0; i<vetor.length;i++)
-        {
-            vetor[i] = new Pessoa();
-            vetor[i].setNome("Chuk Norys");
-            vetor[i].setCPF(1234567890);
-            vetor[i].setIdade(23);
-
-            // Declara e Cria um endereço
-            Endereco e = new Endereco();
-            e.setCidade("California, ca");
-            e.setCEP("10230-400");
-
-            // Atribui o endereço à pessoa
-            vetor[i].setResidencial(e);
+ for(int i=0; i<vetor.length;i++)
+  {
+   vetor[i] = new Pessoa();
+   vetor[i].setNome("Chuk Norys");
+   vetor[i].setCPF(1234567890);
+   vetor[i].setIdade(23);
+   
+   // Declara e Cria um endereço
+   Endereco e = new Endereco();
+   e.setCidade("California, ca");
+   e.setCEP("10230-400");
+   
+   // Atribui o endereço à pessoa
+   vetor[i].setResidencial(e);
             
-            writefln
-             (
-              "\n\tNome: %s"~
-              "\n\tCPF: %s"~
-              "\n\tIdade: %s"~
-              "\n\tCidade: %s"~
-              "\n\tCEP: %s\n",
-              vetor[i].getNome(),
-              vetor[i].getCPF(),
-              vetor[i].getIdade(),
-              e.getCidade(), 
-              e.getCEP()
-             );
-
-        }
-    }
+   writefln(
+            "\n\tNome: %s"~
+            "\n\tCPF: %s"~
+            "\n\tIdade: %s"~
+            "\n\tCidade: %s"~
+            "\n\tCEP: %s\n",
+            vetor[i].getNome(),
+            vetor[i].getCPF(),
+            vetor[i].getIdade(),
+            e.getCidade(), 
+            e.getCEP()
+           );
+  }
+}
